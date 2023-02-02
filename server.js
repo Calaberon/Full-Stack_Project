8ce3f357bbd4327ba3604f41f30ec50ea6415f1b
Caleb Dewey <calebdewey@Calebs-MacBook-Pro.local>
2023-02-01 18:15:19 -0700

    changed default port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { Pool } = require('pg');
 
 dotenv.config()
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const pool = new Pool({
    connectionString: process.env.DATABASE_URL
 });
